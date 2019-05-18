c9a8042aa1a40f4535371aab2520885a2d106246
Oscar Diaz <o-pipe@hotmail.com>
2019-05-17 22:30:15 -0500

    Crea la migracion de cursos con seeds y su controlador y modelo

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('courses')->insert([
+            'name_course' => 'Desarrollo Web',
+            'name_teacher' => 'Pedro Martinez',
+            'capacity' => 10,
+            'observations' => 'Curso desarrollo web donde aprenderas PHP',
+        ]);
+        
+        DB::table('courses')->insert([
+            'name_course' => 'Desarrollo Android',
+            'name_teacher' => 'Julia Perez',
+            'capacity' => 8,
+            'observations' => 'Curso desarrollo donde aprenderas a realizar aplicaciones para andriod',
+        ]);
     }
 }
